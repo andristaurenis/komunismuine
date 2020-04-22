a7cf77bd78d2780977b8337d07c8721d55cfd1cb
Andris Taurenis Server <andris.taurenis@yandex.ru>
2020-04-22 11:55:59 +0000

    deployed

--- hello.py
+++ hello.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     #  return redirect(url_for('lt'))
-    return redirect('/lt/') # everything is so staic right now
+    # return redirect('/lt/') # everything is so staic right now
+    return "hello world"
 
 #  @app.route('/lt/')
 #  def lt():
     #  return url_for('static', filename='index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
