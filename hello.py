13aaaffa27c00997be35950ee1e921407d20b019
Peteris Ratnieks <peteris@factury.co>
2020-04-23 08:32:02 +0300

    add catchall route

--- hello.py
+++ hello.py
@@ -1,5 +1,18 @@
+from os.path import join
 from flask import Flask, redirect, url_for, flash
+
 app = Flask(__name__)
+
+static_files = ['index.css', 'README.md', '/static/index-en.html']
+@app.route('/<path:any_path>')
+def serve_static_folder(any_path):
+    assumed_static_path = join('/static', any_path)
+    #  app.logger.debug(request.base_url)
+    app.logger.debug(any_path)
+    if assumed_static_path in static_files:
+        return "yay"
+        #  return app.send_static_file(assumed_static_path)
+    return {any_path: assumed_static_path}
 
 @app.route('/')
 def hello_world():
@@ -13,4 +26,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
